0967a9dd0712c8ea583906600a60fda32f87bf6a
andreworba01 <136288676+andreworba01@users.noreply.github.com>
2025-10-13 12:04:33 -0500

    Update app.v1.py

--- app.v1.py
+++ app.v1.py
@@ -176,7 +176,7 @@ elif page == "🗺️ County Risk Map":
         "Baseline (Current Conditions)": "mean_score",
         "Scenario 1 — 50% do not use plastic containers in microwave": "Decrease_50",
         "Scenario 2 — 95% do not use plastic containers in microwave": "Decrease_95",
-        "Scenario 3 — 20% use plastic containers ≥5× per day": "Increase_20 ",
+        "Scenario 3 — 20% use plastic containers ≥5× per day": "Increase_20",
         "Scenario 4 — 50% use plastic containers ≥5× per day": "Increase_50",
     }
     score_col = scenario_map[scenario_label]
